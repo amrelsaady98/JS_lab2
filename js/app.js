b472726504950df34a7d6fdb9c5ddf0da03ec2da
amrelsaady98 <amrelsaady98@hotmail.com>
2024-05-29 10:58:08 +0300

    modify date validation @app.js

--- js/app.js
+++ js/app.js
@@ -74,7 +74,7 @@ try {
           if (Number.parseInt(dateArray[j]) > 12 || Number.parseInt(dateArray[j]) < 1) {
             return "Month Must be in range of 1 ~ 12";
           } else {
-            dateObject.setMonth(Number.parseInt(dateArray[j]))
+            dateObject.setMonth(Number.parseInt(dateArray[j]) + 1)
           }
           break;
         case 2:
